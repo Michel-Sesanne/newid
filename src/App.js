e10cceb7e11d3a397a8c6a1b228ca2fb5212c55e
Michel Sesanne <msesanne@gmail.com>
2024-06-13 21:21:41 +0200

    ajustement 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 export default function App() {
   return (
     <HelmetProvider>
-      <Router>
+      <Router basename="/newid">
         <ScrollToTop />
         <Layout>
           <JsonLd />
--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -5,15 +5,12 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const headerHeight = document.getElementById('header')?.offsetHeight || 0; // Récupère la hauteur du header
-
-    // Si la route est '/' ou '*', on scrolle jusqu'en haut de la page
-    // Sinon, on scrolle jusqu'à la hauteur du header
-    const yOffset = ['/', '*'].includes(pathname) ? 0 : headerHeight;
-
+    const headerHeight = document.getElementById('header')?.offsetHeight || 0;
+    const yOffset = (pathname === '/' || pathname === '*') ? 0 : headerHeight;
+    
     window.scrollTo({
       top: yOffset,
-      behavior: 'smooth' // Défilement fluide
+      behavior: 'smooth',
     });
   }, [pathname]);
 
