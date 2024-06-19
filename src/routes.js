b3a1afe2ac9f6f7a441db84605a6e945d8865cec
Michel Sesanne <msesanne@gmail.com>
2024-06-20 00:21:16 +0200

    Ajout du HashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import TarifsElectricien from './pages/TarifsElectricien/TarifsElectricien';
 import TarifsSerrurier from './pages/TarifsSerrurier/TarifsSerrurier';
 import MentionsLegales from './pages/MentionsLegales/MentionsLegales';
 
 export default function AppRoutes() {
-    return (
-        <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/tarifsElectricien" element={<TarifsElectricien />} />
-           <Route path="/tarifsSerrurier" element={<TarifsSerrurier />} />
-           <Route path="/mentionsLegales" element={<MentionsLegales />} />
-           <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tarifsElectricien" element={<TarifsElectricien />} />
+        <Route path="/tarifsSerrurier" element={<TarifsSerrurier />} />
+        <Route path="/mentionsLegales" element={<MentionsLegales />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
