33d73db54831e43ef228328ac005f653f13e98dc
Michel Sesanne <msesanne@gmail.com>
2024-06-20 14:48:44 +0200

    Annulation HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Layout from './components/Layout/Layout';
 import AppRoutes from './routes';
